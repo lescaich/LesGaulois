06ff64a7c6f8e17780dd728958191afa7680d938
lescaich <lescaich@btslivh.eu>
2024-01-24 09:53:02 +0100

    TP Q4

--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -5,5 +5,6 @@ import personnages.Gaulois;
 public class TestGaulois {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix",8);
+		System.out.println(asterix.getNom());
 	}
 }
