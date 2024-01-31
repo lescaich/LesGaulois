b775b1fab76044d54ec4ac408ebea64f82c3f18c
lescaich <lescaich@btslivh.eu>
2024-01-31 09:25:30 +0100

    TP1 Q7

---
+++ src/personnages/Romain.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Romain {
+	
+	private String nom; 
+	public int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+}
