8bf754900744cc9dc84b33c730add75cadcde4c7
lescaich <lescaich@btslivh.eu>
2024-02-28 09:54:18 +0100

    [EN COURS] TP2 3d Ajout méthode ajouterSoldat (non fonctionnel)

--- src/sites/Camp.java
+++ src/sites/Camp.java
@@ -5,15 +5,41 @@ import personnages.Soldat;
 public class Camp {
 	
 	private Soldat commandent;
+	private int nbSoldats = 0;
+	private Soldat[] listeSoldats = new Soldat[4];
 	
 	public Camp(Soldat commandent) {
 		super();
 		this.commandent = commandent;
+		listeSoldats[0] = this.commandent;
+		nbSoldats++;
 		commandent.parler("Je suis en charge de créer un nouveau camp romain ");
 	}
 
 	public Soldat getCommandent() {
 		return commandent;
+	}
+	
+	public void afficherListeSoldats() {
+		for (int i=0; i < nbSoldats; i++) {
+			System.out.println(listeSoldats[i].getNom());
+		}
+	}
+
+	public void setListeSoldats(Soldat[] listeSoldats) {
+		this.listeSoldats = listeSoldats;
+	}
+
+	public boolean ajouterSoldat(Soldat soldat) {
+		boolean isAjoute = false;
+		for (int i=0; i < nbSoldats; i++) {
+			if (listeSoldats[i] == null) {
+				listeSoldats[i] = soldat;
+				nbSoldats++;
+				isAjoute = true;
+			}
+		}
+		return isAjoute;
 	}
 	
 }
--- src/test_fonctionnel/TestSites.java
+++ src/test_fonctionnel/TestSites.java
@@ -13,9 +13,15 @@ public class TestSites {
 		Gaulois vercingetorix = new Gaulois("Vercingétorix",5);
 		Soldat minus = new Soldat("Minus",2);
 		
+		Soldat brutus = new Soldat("Brutus",5);
+		
 		// Création des sites
 		Village village1 = new Village(vercingetorix);
 		Camp camp1 = new Camp(minus);
+		
+		camp1.afficherListeSoldats();
+		camp1.ajouterSoldat(brutus);
+		camp1.afficherListeSoldats();
 		
 	}
 	
