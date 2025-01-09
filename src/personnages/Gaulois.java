f03729b3155f53ec13a28bd582973915a716f90d
lescaich <lescaich@btslivh.eu>
2025-01-09 09:18:20 +0100

    TP1 1&2 Implémentation package personnages et class Gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Gaulois {
+	String nom;
+	int force;
+	
+	Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	String getNom() {
+		return nom;
+	}
+}
