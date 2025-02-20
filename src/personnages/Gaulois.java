7ea038c6274a00afd66255116f019d818de82d95
lescaich <lescaich@btslivh.eu>
2025-02-20 11:59:41 +0100

    TP1 10 FIN Ajout méthode frapper dans la class Gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -17,6 +17,12 @@ public class Gaulois {
 		System.out.println("Le gaulois " + nom + " : \" " + texte + " \". ");
 	}
 	
+	public void frapper(Romain romain) {
+		System.out.println(nom + "  envoie un grand coup dans la mâchoire de " + romain.getNom() + ". ");
+		romain.recevoirCoup(force/3);
+	}
+	
+	
 	/*public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		System.out.println(asterix.getNom());
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -12,8 +12,11 @@ public class TestGaulois {
 		
 		Romain minus = new Romain("Minus", 6);
 		minus.parler("UN GAU... UN GAUGAU...");
-		for(int i=0; i<2; i++) {
+		/*for(int i=0; i<2; i++) {
 			minus.recevoirCoup(3);
+		}*/
+		for(int i=0; i<3; i++) {
+			asterix.frapper(minus);
 		}
 	}
 }
